163f1939a29911a9a0857e052338f6603bdba6d7
Ondřej Kraus <okraus42+github@pm.me>
2023-03-05 17:38:20 +0100

    main

--- main.c
+++ main.c
@@ -25,10 +25,10 @@ int	main(void)
 
 	c = 'a';
 	d = 'b';
-	num = 2147483647;
-	u = 4294967294;
-	pf = printf("printf : a%pbc%%de%5cfg%-20.15uhij%2sk%-+20.15dlm% inop%#20.15Xq%xr\n", (void *)&c, d, u, s, num, 0, u, u);
-	fp = ft_printf("ftprint: a%pbc%%de%5cfg%-20.15uhij%2sk%-+20.15dlm% inop%#20.15Xq%xr\n", (void *)&c, d, u, s, num, 0, u, u);
+	num = 0x7FFFFFFF;
+	u = 0xFFFFFFFF;
+	pf = printf("printf : a%pbc%%de%5cfg%-20.15uhij%2sk%-+20.15dlm% inop%#20.15Xq%#xr\n", (void *)&c, d, u, s, num, 0, u, u);
+	fp = ft_printf("ftprint: a%pbc%%de%5cfg%-20.15uhij%2sk%-+20.15dlm% inop%#20.15Xq%#xr\n", (void *)&c, d, u, s, num, 0, u, u);
 	printf ("pf = %d, fp = %d\n", pf, fp);
 	pf = printf("%.3d\n", -1234);
 	fp = ft_printf("%.3d\n", -1234);
@@ -38,6 +38,16 @@ int	main(void)
 	printf ("pf = %d, fp = %d\n", pf, fp);
 	pf = printf("%8p-%8s\n", NULL, s2);
 	fp = ft_printf("%8p-%8s\n", NULL, s2);
+	printf ("pf = %d, fp = %d\n", pf, fp);
+
+	pf = printf("%i|%i|%i\n", -2147483648, 0, 0x7FFFFFFF);
+	fp = ft_printf("%i|%i|%i\n", -2147483648, 0, 0x7FFFFFFF);
+	printf ("pf = %d, fp = %d\n", pf, fp);
+	pf = printf("%p-%s\n", NULL, s2);
+	fp = ft_printf("%p-%s\n", NULL, s2);
+	printf ("pf = %d, fp = %d\n", pf, fp);
+	pf = printf("printf : a%pbc%%de%cfg%uhij%2sk%dlm%inop%Xq%xr\n", (void *)&c, d, u, s, num, 0, u, u);
+	fp = ft_printf("ftprint: a%pbc%%de%cfg%uhij%2sk%dlm%inop%Xq%xr\n", (void *)&c, d, u, s, num, 0, u, u);
 	printf ("pf = %d, fp = %d\n", pf, fp);
 	return (0);
 }
